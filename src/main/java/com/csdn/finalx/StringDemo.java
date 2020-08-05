8fb2656b029d3acf82427c4c334f0877e3582465
xwf <420842659@qq.com>
2020-08-05 22:11:56 +0800

    final 示例

---
+++ src/main/java/com/csdn/finalx/StringDemo.java
@@ -0,0 +1,18 @@
+package com.csdn.finalx;
+
+/**
+ * @author ：xwf
+ * @date ：Created in 2020\8\5 0005 21:39
+ */
+public class StringDemo {
+    public static void main(String[] args) {
+        String s = "csdn";
+        s = "cs";
+        System.out.println(s);
+    }
+
+    void demo1(){
+        String s1="csdn";
+        String s2="csdn";
+    }
+}
